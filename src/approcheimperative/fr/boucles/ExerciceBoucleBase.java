3cfb8a12caa518bf3c3ae13a558d9dd4d893c9eb
Louis Bernardeau <lbernardeau@la-joliverie.com>
2020-11-18 13:11:22 +0100

    commit exo 1

---
+++ src/approcheimperative/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,25 @@
+package approcheimperative.fr.boucles;
+
+public class ExerciceBoucleBase {
+    public static void main ( String[] args ) {
+        /*
+        for (int i=1; i<11;i++) {
+            System.out.println(i);
+        }*/
+
+        /*
+        String nom = "Bernardeau";
+        String prenom = "Louis";
+        for (int i=0; i<20;i++) {
+            System.out.println(i+nom + prenom);
+        }*/
+
+        /*for (int i=2; i<102;i=i+2) {
+            System.out.println(i);
+        }*/
+
+        for (int i=1; i<101;i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
