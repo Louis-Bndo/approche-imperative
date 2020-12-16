1b3a6a7376249b9ecb1575e3c41040856eff8875
Louis Bernardeau <lbernardeau@la-joliverie.com>
2020-12-16 15:39:24 +0100

    modif

---
+++ src/listes/Ville.java
@@ -0,0 +1,12 @@
+package listes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Ville {
+    String nom;
+    int nb_habitant;
+    List<Ville> listeVille = new ArrayList<Ville>();
+    //Collections.addAll(listeVille, "Nice", "Carcassonne", "Narbonne", "Lyon", "Foix", "Pau", "Marseille", "Tarbes");
+}
--- src/set/TestSetString.java
+++ src/set/TestSetString.java
@@ -2,15 +2,19 @@ package set;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class TestSetString {
     public static void main(String[] args) {
         Set<String> setString = new HashSet<String>();
         Collections.addAll(setString, "USA", "France", "Allemagne", "UK", "Italie", "Japon", "Chine", "Russie", "Inde");
-
         String plusGrand = Collections.max(setString);
         setString.remove(plusGrand);
-        System.out.println(setString);
+        Iterator i=setString.iterator(); // on crée un Iterator pour parcourir notre HashSet
+        while(i.hasNext()) // tant qu'on a un suivant
+        {
+            System.out.println(i.next()); // on affiche le suivant
+        }
     }
 }
